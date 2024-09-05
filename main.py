9a66025a41885ee869e022633e4c4c6e44ae4207
uino59 <uino58@yahoo.com>
2024-09-05 09:53:08 +0200

    add circleshape class and player object

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
 from constants import *
+from player import *
 
 running = True
-
 
 def main():
     print("Starting asteroids!")
@@ -11,7 +11,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
 
     while running:
@@ -20,7 +22,11 @@ def main():
                 return
 
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
